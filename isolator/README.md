d659c17ef3541b40dcbe9956cbc19e0e87c26c5a
Kapil Arya <kapil@mesosphere.io>
2015-04-17 17:22:20 -0400

    Initial import.

---
+++ isolator/README.md
@@ -0,0 +1,63 @@
+#Metaswitch Network Isolation Module
+
+This module allows one to execute a given command in the network namespace
+before and after launching tasks/executors.
+
+##Build
+See [Building the Modules](https://github.com/mesosphere/mesos-modules-private).
+
+##Setup
+
+Create a JSON file for specifying the modules for the slave as per the
+[template](modules.json.in).
+
+
+This module accepts two parameters:
+1. `initialization_command`: This command (along with arguments) is executed
+   in the container context after the network namespace has been created
+   but before launching the tasks.
+2. `cleanup_command`: This command is executed right before the network
+   namespace is taken down.
+
+###Example JSON file:
+```
+{
+  "libraries": [
+    {
+      "file": "/path/to/libmetaswitch_network_isolator.so
+      "modules": [
+        {
+          "name": "com_mesosphere_mesos_MetaswitchNetworkIsolator",
+          "parameters": [
+            {
+              "key": "initialization_command",
+              "value": "python /path/to/initialization_script.py arg1 arg2"
+            },
+            {
+              "key": "cleanup_command",
+              "value": "python /path/to/cleanup_script.py arg1 arg2"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+```
+
+### TODO: Optional arguments
+
+If needed, we can add two more parameters -- `initialization_args` and
+`cleanup_args` to explicitly supply arguments to the corresponding commands.
+
+
+##Use
+
+The module is only useful for the slave. In order to use this module, one needs
+to specify two flags -- `modules` and `isolation`. Here is an example:
+
+
+```
+./bin/mesos-slave.sh --master=master_ip:port --modules=file://path/to/slave_gssapi.json \
+    --isolation="com_mesosphere_mesos_MetaswitchNetworkIsolator"
+```
---
+++ isolator/network_isolator.cpp
@@ -0,0 +1,141 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <mesos/mesos.hpp>
+#include <mesos/module.hpp>
+
+#include <mesos/module/isolator.hpp>
+
+#include <mesos/slave/isolator.hpp>
+
+#include <process/future.hpp>
+#include <process/owned.hpp>
+#include <process/subprocess.hpp>
+
+#include <stout/try.hpp>
+
+using namespace mesos;
+
+using mesos::slave::Isolator;
+
+const char* initializationKey = "initialization_command";
+const char* cleanupKey = "cleanup_command";
+
+class MetaswitchNetworkIsolatorProcess : public mesos::slave::IsolatorProcess
+{
+public:
+  static Try<mesos::slave::Isolator*> create(const Parameters& parameters)
+  {
+    return new Isolator(process::Owned<IsolatorProcess>(
+        new MetaswitchNetworkIsolatorProcess(parameters)));
+  }
+
+  virtual ~MetaswitchNetworkIsolatorProcess() {}
+
+  virtual process::Future<Nothing> recover(
+      const std::list<mesos::slave::ExecutorRunState>& states)
+  {
+    return Nothing();
+  }
+
+  virtual process::Future<Option<CommandInfo>> prepare(
+      const ContainerID& containerId,
+      const ExecutorInfo& executorInfo,
+      const std::string& directory,
+      const Option<std::string>& user)
+  {
+    LOG(INFO) << "MetaswitchNetworkIsolator::prepare";
+    foreach (const Parameter& parameter, parameters.parameter()) {
+      if (parameter.key() == initializationKey) {
+        CommandInfo commandInfo;
+        commandInfo.set_value(parameter.value());
+        return commandInfo;
+      }
+    }
+    return None();
+  }
+
+  virtual process::Future<Nothing> isolate(
+      const ContainerID& containerId,
+      pid_t pid)
+  {
+    return Nothing();
+  }
+
+  virtual process::Future<mesos::slave::Limitation> watch(
+      const ContainerID& containerId)
+  {
+    return process::Future<mesos::slave::Limitation>();
+  }
+
+  virtual process::Future<Nothing> update(
+      const ContainerID& containerId,
+      const Resources& resources)
+  {
+    return Nothing();
+  }
+
+  virtual process::Future<ResourceStatistics> usage(
+      const ContainerID& containerId)
+  {
+    return ResourceStatistics();
+  }
+
+  virtual process::Future<Nothing> cleanup(
+      const ContainerID& containerId)
+  {
+    foreach (const Parameter& parameter, parameters.parameter()) {
+      if (parameter.key() == cleanupKey) {
+        Try<process::Subprocess> child = process::subprocess(parameter.value());
+        CHECK_SOME(child);
+        waitpid(child.get().pid(), NULL, 0);
+        break;
+      }
+    }
+    return Nothing();
+  }
+
+private:
+  MetaswitchNetworkIsolatorProcess(const Parameters& parameters_)
+    : parameters(parameters_) {}
+
+  const Parameters parameters;
+};
+
+
+static Isolator* createMetaswitchNetworkIsolator(const Parameters& parameters)
+{
+  LOG(INFO) << "Loading Metaswitch Network Isolator module";
+  Try<Isolator*> result = MetaswitchNetworkIsolatorProcess::create(parameters);
+  if (result.isError()) {
+    return NULL;
+  }
+  return result.get();
+}
+
+
+// Declares the Metaswitch network isolator named
+// 'org_apache_mesos_MetaswitchNetworkIsolator'.
+mesos::modules::Module<Isolator> com_mesosphere_mesos_MetaswitchNetworkIsolator(
+    MESOS_MODULE_API_VERSION,
+    MESOS_VERSION,
+    "Mesosphere",
+    "support@mesosphere.com",
+    "Metaswitch Network Isolator module.",
+    NULL,
+    createMetaswitchNetworkIsolator);
