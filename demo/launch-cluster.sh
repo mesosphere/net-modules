a09751f9e982c4c1ad87a0dc8cca50a2ff3148d0
Connor Doyle <connor@mesosphere.io>
2015-07-31 18:34:02 -0700

    Added scripts and Marathon JSON to support demo.

---
+++ demo/launch-cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+set -o errexit -o nounset -o pipefail
+
+DEMO_DIR=`dirname $0`
+PROJECT_DIR=$DEMO_DIR/..
+
+echo "Launching cluster with network isolation modules enabled..."
+
+docker-compose -p modules up -d
+
+$PROJECT_DIR/add-container-route.sh
---
+++ demo/stop-stars-isolated.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+set -o errexit -o nounset -o pipefail
+
+echo "Destroying group 'stars'"
+
+curl -X DELETE http://$(boot2docker ip):8080/v2/groups/star-isolated?force=true
