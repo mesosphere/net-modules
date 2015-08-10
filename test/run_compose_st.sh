927e2bbab7559bfea2fc7ff5816edc7448b716f7
Dan Osborne <daniel.osborne@metaswitch.com>
2015-08-10 11:03:12 -0700

    added python scripts determine which check which apps are running on which slaves

--- test/run_compose_st.sh
+++ test/run_compose_st.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 docker-compose -p modules up -d
+cd `dirname $0`
 
 # Wait for the marathon container to come up
 sleep 60
-curl -X POST http://localhost:8080/v2/apps -d @`dirname $0`/sample-flask-app.json -H "Content-type: application/json"
-curl -X POST http://localhost:8080/v2/apps -d @`dirname $0`/sample-flask-app-2.json -H "Content-type: application/json"
+curl -X POST http://localhost:8080/v2/apps -d @sample-flask-app.json -H "Content-type: application/json"
+curl -X POST http://localhost:8080/v2/apps -d @sample-flask-app-2.json -H "Content-type: application/json"
 
 # Wait for calico-node image to download
 sleep 90
 
-# ping sample application to see if calico networking worked
-docker exec modules_slave1_1 ping -c 4 192.168.0.2
-docker exec modules_slave2_1 ping -c 4 192.168.0.3
+curl -o state.json localhost:5050/master/state.json
 
-# Ping the sample apps
-docker exec modules_slave1_1 ping -c 4 container.sample-flask-app.marathon.mesos
-docker exec modules_slave2_1 ping -c 4 container.sample-flask-app-2.marathon.mesos
+# Make sure slave1 can ping the IP of the app running on it
+docker exec modules_slave1_1 ping -c 4 `python get_ip_on_app.py slave1`
+
+# Make sure slave2 can ping the IP of the app running on it
+docker exec modules_slave2_1 ping -c 4 `python get_ip_on_app.py slave2`
+
+# Slave1 should be able to ping the hostname of the app running on it
+docker exec modules_slave1_1 ping -c 4 container.`python get_app_on_slave.py slave1`.marathon.mesos
+
+# Slave2 should be able to ping the hostname of the app running on it
+docker exec modules_slave2_1 ping -c 4 container.`python get_app_on_slave.py slave2`.marathon.mesos
